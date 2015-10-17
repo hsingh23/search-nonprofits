f42f8e1963cb17875f5f665c009a1254b4007767
Harsh Singh <hisingh1@gmail.com>
2015-10-16 23:33:13 -0400

    feat: add search form UI and structured /api/ results endpoint
    Introduce a Nonprofit class that parses CSV rows into searchable
    fields and serve a Bootstrap index page whose form queries GET /api/
    by name, city, and state. Results render via a new api_results.html
    template split into email, website, and optional no-contact groups.
    Add none.csv as a third dataset and refresh emails.csv/websites.csv.

--- app.py
+++ app.py
@@ -1,55 +1,65 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 emails = []
 websites = []
+none = []
+
+class Nonprofit(object):
+	"""docstring for Nonprofit"""
+	def __init__(t, nonprofit_string):
+		super(Nonprofit, t).__init__()
+		properties = ["ein","name","url","year","person","addr1","addr2","city","state"]
+		nonprofit_details = nonprofit_string.split("|")
+		t.string = nonprofit_string
+		t.ein = nonprofit_details[0].strip()
+		t.name = nonprofit_details[1].strip()
+		t.url = nonprofit_details[2].strip().lower()
+		t.year = nonprofit_details[3].strip()
+		t.person = nonprofit_details[4].strip()
+		t.addr1 = nonprofit_details[5].strip()
+		t.addr2 = nonprofit_details[6].strip()
+		t.city = nonprofit_details[7].strip()
+		t.state = nonprofit_details[8].upper().strip()
+		t.full_addr = "{0} {1}, {2}, {3}".format(t.addr1, t.addr2, t.city, t.state)
+		t.ein = "0"+t.ein  if (len(t.ein)==8) else t.ein
+
+	def match(t, name, city, state):
+		res =  t.name.lower().find(name.lower()) > -1 and \
+			t.city.lower().find(city.lower()) > -1 and \
+			t.state.lower().find(state.lower()) > -1
+		return res
+
 
 with open("emails.csv", "r") as f:
-    emails = [ l.strip().decode("utf8") for l in f.readlines()]
+    emails = [ Nonprofit(l.strip().decode("utf8")) for l in f.readlines() if len(l) > 5]
 
 with open("websites.csv", "r") as f:
-    websites = [ l.decode("utf8").strip() for l in f.readlines()]
+    websites = [ Nonprofit(l.decode("utf8").strip()) for l in f.readlines() if len(l) > 5]
+
+with open("none.csv", "r") as f:
+    none = [ Nonprofit(l.strip().decode("utf8")) for l in f.readlines() if len(l) > 5]
+
 
 @app.route('/')
 def index():
-    return 'Search nonprofits with email and website by adding your search term after the / <br> You can also search a city using /city/City Name'
+	return render_template("index.html")
 
-@app.route('/<search>')
-def simple_search(search):
-    search = search.lower()
-    filtered_emails = "<br>".join([ l for l in emails if search in l.lower()  ])
-    filtered_websites = "<br>".join([ l for l in websites if search in l.lower()  ])
-
-    matches = """
-    <h2> ein(IRS identification number)|organization_name|officer_address_city|organization_address_state|officer_name|email </h2>
-    <h1> Emails </h1>
-    %s
-    <br>
-    <br>
-    <h1> Websites </h1>
-    %s
-    """ % (filtered_emails, filtered_websites)
-    return matches
-
-@app.route('/city/<search>')
-def city_search(search):
-    search = "|"+search.lower()+"|"
-    filtered_emails = "<br>".join([ l for l in emails if search in l.lower()  ])
-    filtered_websites = "<br>".join([ l for l in websites if search in l.lower()  ])
-
-    matches = """
-    <h2> ein(IRS identification number)|organization_name|officer_address_city|organization_address_state|officer_name|email </h2>
-    <h1> Emails </h1>
-    %s
-    <br>
-    <br>
-    <h1> Websites </h1>
-    %s
-    """ % (filtered_emails, filtered_websites)
-    return matches
-
-
+@app.route('/api/', methods=['GET'])
+def api():
+	name = request.args.get('name', "")
+	city = request.args.get('city', "")
+	state = request.args.get('state', "")
+	if (name == "" and city == "" and state == ""):
+		return "<b> You must specify a name, city or state</b>"
+	include_none = request.args.get('include_none', False)
+	my_emails = [nonprofit for nonprofit in emails if nonprofit.match(name, city, state)]
+	my_websites = [nonprofit for nonprofit in websites if nonprofit.match(name, city, state)]
+	my_none = [nonprofit for nonprofit in none if nonprofit.match(name, city, state)] if include_none else []
+	return render_template("api_results.html", my_emails=my_emails, my_websites=my_websites, my_none=my_none, include_none=include_none)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9000, debug=True)
